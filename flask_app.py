0e4a4ff25eb39b3134baccba6ae2e948ddc51ec6
pablodeas <pablodeas@gmail.com>
2025-03-17 14:07:02 -0300

    changing 002

--- flask_app.py
+++ flask_app.py
@@ -1,9 +1,44 @@
-from flask import Flask, render_template
+import os
+from load_dotenv import load_dotenv
+from flask import Flask, redirect, render_template, request, url_for
+from flask_sqlalchemy import SQLAlchemy
+
+load_dotenv()
+
+USERNAME = "pablodeas"
+PASSWORD = os.getenv("PASSWORD")
+HOSTNAME = os.getenv("HOSTNAME")
+DATABASENAME = "pablodeas"
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
 
-@app.route('/')
-def index():
-    return render_template("main_page.html")
+SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
+    username=USERNAME,
+    password=PASSWORD,
+    hostname=HOSTNAME,
+    databasename=DATABASENAME,
+)
+app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
+app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
+db = SQLAlchemy(app)
+
+class Comment(db.Model):
+
+    __tablename__ = "comments"
+
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(4096))
+
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "GET":
+        return render_template("main_page.html", comments=Comment.query.all())
+
+    comment = Comment(content=request.form["contents"])
+    db.session.add(comment)
+    db.session.commit()
+    return redirect(url_for('index'))
