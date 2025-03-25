97d22ec8a472767b78dc2f2acdcc886afe6d2921
pablodeas <pablodeas@gmail.com>
2025-03-25 19:13:59 -0300

    new: first version

--- flask_app.py
+++ flask_app.py
@@ -1,23 +1,22 @@
 import os
-from load_dotenv import load_dotenv
+from dotenv import load_dotenv
 from flask import Flask, redirect, render_template, request, url_for
 from flask_sqlalchemy import SQLAlchemy
 
-load_dotenv()
-
-USERNAME = "pablodeas"
-PASSWORD = os.getenv("PASSWORD")
-HOSTNAME = os.getenv("HOSTNAME")
-DATABASENAME = "pablodeas$default"
-
 app = Flask(__name__)
-app.config["DEBUG"] = True
+load_dotenv()
+app.config["DEBUG"] = os.getenv("FLASK_DEBUG", "False").lower() in ["true", "1"]
+
+USERNAME = os.getenv("DB_USERNAME")
+PASSWORD = os.getenv("DB_PASSWORD")
+HOSTNAME = os.getenv("DB_HOSTNAME")
+DATABASE = os.getenv("DB_DATABASE")
 
 SQLALCHEMY_DATABASE_URI = "mysql+mysqlconnector://{username}:{password}@{hostname}/{databasename}".format(
     username=USERNAME,
-    password="Maitou2656",
-    hostname="pablodeas.mysql.pythonanywhere-services.com",
-    databasename=DATABASENAME,
+    password=PASSWORD,
+    hostname=HOSTNAME,
+    databasename=DATABASE,
 )
 app.config["SQLALCHEMY_DATABASE_URI"] = SQLALCHEMY_DATABASE_URI
 app.config["SQLALCHEMY_POOL_RECYCLE"] = 299
@@ -25,20 +24,16 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
 
-class Comment(db.Model):
-
-    __tablename__ = "comments"
-
+class Vaga(db.Model):
+    __tablename__ = "vaga"
     id = db.Column(db.Integer, primary_key=True)
-    content = db.Column(db.String(4096))
-
+    conteudo = db.Column(db.String(4096))
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "GET":
-        return render_template("main_page.html", comments=Comment.query.all())
-
-    comment = Comment(content=request.form["contents"])
-    db.session.add(comment)
+        return render_template("main_page.html", vaga=Vaga.query.all())
+    vaga = Vaga(conteudo=request.form["conteudo"])
+    db.session.add(vaga)
     db.session.commit()
-    return redirect(url_for('index'))
+    return redirect(url_for("index"))
